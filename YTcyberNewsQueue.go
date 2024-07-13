1b69fcc2daac8cf8d126d8552bb76d086216760f
r00t <mkthun@protonmail.com>
2024-07-13 18:18:34 +0100

    KISSing it

---
+++ YTcyberNewsQueue.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+)
+
+// https://github.com/ChrisPritchard/ctf-writeups/blob/master/GO-SCRIPTING.md
+
+const (
+	cybernews  string = "https://www.youtube.com/@cybernews/videos"
+	seytonic   string = "https://www.youtube.com/@Seytonic/videos"
+	hakfive    string = "https://www.youtube.com/@hak5/videos"
+	firefoxCmd string = "firefox --new-window "
+)
+
+func main() {
+	urlsBuilder := strings.Builder{}
+	urlsBuilder.WriteString(cybernews + " " + seytonic + " " + hakfive)
+	allUrls := urlsBuilder.String()
+
+	openAndCheckNewVideos := exec.Command(firefoxCmd, allUrls)
+	err := openAndCheckNewVideos.Run()
+	if nil != err {
+		panic(err)
+	}
+}
